63ebf7122fc69e90cc997fb84fbe833897daa3ae
oleg3479881328-code <oleg3479881328@gmail.com>
2026-07-10 17:14:34 -0400

    Handle profile metadata updates

--- projects/tiktok-research-sorter/entrypoints/background.ts
+++ projects/tiktok-research-sorter/entrypoints/background.ts
@@ -1,29 +1,27 @@
 import { browser } from 'wxt/browser';
-import { clearProfile, loadDashboard, mergeVideoBatch, updateScanState } from '../lib/store';
+import { clearProfile, loadDashboard, mergeProfileData, mergeVideoBatch, updateScanState } from '../lib/store';
 import type { RuntimeMessage } from '../lib/types';
 
 export default defineBackground(() => {
   browser.sidePanel?.setPanelBehavior({ openPanelOnActionClick: true }).catch(() => undefined);
 
   browser.runtime.onMessage.addListener((message: RuntimeMessage) => {
+    const publish = <T,>(promise: Promise<T>) => promise.then((dashboard) => {
+      void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard }).catch(() => undefined);
+      return dashboard;
+    });
+
     switch (message.type) {
       case 'GET_DASHBOARD':
         return loadDashboard();
+      case 'PROFILE_DATA':
+        return publish(mergeProfileData(message.profile));
       case 'VIDEO_BATCH':
-        return mergeVideoBatch(message.username, message.profileUrl, message.videos).then((dashboard) => {
-          void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard }).catch(() => undefined);
-          return dashboard;
-        });
+        return publish(mergeVideoBatch(message.username, message.profileUrl, message.videos));
       case 'SCAN_STATE':
-        return updateScanState(message.state).then((dashboard) => {
-          void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard }).catch(() => undefined);
-          return dashboard;
-        });
+        return publish(updateScanState(message.state));
       case 'CLEAR_PROFILE':
-        return clearProfile(message.username).then((dashboard) => {
-          void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard }).catch(() => undefined);
-          return dashboard;
-        });
+        return publish(clearProfile(message.username));
       default:
         return undefined;
     }
