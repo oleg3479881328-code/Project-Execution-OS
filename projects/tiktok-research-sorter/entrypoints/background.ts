a0da6ad095c93c6c7101718bb57581bf01a94dd9
oleg3479881328-code <oleg3479881328@gmail.com>
2026-07-12 10:10:08 -0400

    Serialize TikTok dashboard mutations

--- projects/tiktok-research-sorter/entrypoints/background.ts
+++ projects/tiktok-research-sorter/entrypoints/background.ts
@@ -1,27 +1,35 @@
 import { browser } from 'wxt/browser';
 import { clearProfile, loadDashboard, mergeProfileData, mergeVideoBatch, updateScanState } from '../lib/store';
-import type { RuntimeMessage } from '../lib/types';
+import type { DashboardData, RuntimeMessage } from '../lib/types';
 
 export default defineBackground(() => {
   browser.sidePanel?.setPanelBehavior({ openPanelOnActionClick: true }).catch(() => undefined);
 
-  browser.runtime.onMessage.addListener((message: RuntimeMessage) => {
-    const publish = <T,>(promise: Promise<T>) => promise.then((dashboard) => {
-      void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard }).catch(() => undefined);
-      return dashboard;
-    });
+  let mutationQueue: Promise<void> = Promise.resolve();
 
+  const enqueue = <T,>(operation: () => Promise<T>): Promise<T> => {
+    const next = mutationQueue.then(operation, operation);
+    mutationQueue = next.then(() => undefined, () => undefined);
+    return next;
+  };
+
+  const publish = (operation: () => Promise<DashboardData>) => enqueue(operation).then((dashboard) => {
+    void browser.runtime.sendMessage({ type: 'DASHBOARD_UPDATED', dashboard } satisfies RuntimeMessage).catch(() => undefined);
+    return dashboard;
+  });
+
+  browser.runtime.onMessage.addListener((message: RuntimeMessage) => {
     switch (message.type) {
       case 'GET_DASHBOARD':
-        return loadDashboard();
+        return enqueue(loadDashboard);
       case 'PROFILE_DATA':
-        return publish(mergeProfileData(message.profile));
+        return publish(() => mergeProfileData(message.profile));
       case 'VIDEO_BATCH':
-        return publish(mergeVideoBatch(message.username, message.profileUrl, message.videos));
+        return publish(() => mergeVideoBatch(message.username, message.profileUrl, message.videos));
       case 'SCAN_STATE':
-        return publish(updateScanState(message.state));
+        return publish(() => updateScanState(message.state));
       case 'CLEAR_PROFILE':
-        return publish(clearProfile(message.username));
+        return publish(() => clearProfile(message.username));
       default:
         return undefined;
     }
