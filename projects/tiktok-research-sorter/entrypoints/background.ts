6eb7ae7ee1bcbb0952670e2313d95ec2f139c23b
oleg3479881328-code <oleg3479881328@gmail.com>
2026-07-13 19:55:35 -0400

    Route hashtag scan messages through serialized background storage

--- projects/tiktok-research-sorter/entrypoints/background.ts
+++ projects/tiktok-research-sorter/entrypoints/background.ts
@@ -1,5 +1,14 @@
 import { browser } from 'wxt/browser';
-import { clearProfile, loadDashboard, mergeProfileData, mergeVideoBatch, updateScanState } from '../lib/store';
+import {
+  beginTagResearch,
+  clearProfile,
+  clearTagResearch,
+  loadDashboard,
+  mergeProfileData,
+  mergeTagVideoBatch,
+  mergeVideoBatch,
+  updateScanState,
+} from '../lib/store';
 import type { DashboardData, RuntimeMessage } from '../lib/types';
 
 export default defineBackground(() => {
@@ -26,10 +35,16 @@ export default defineBackground(() => {
         return publish(() => mergeProfileData(message.profile));
       case 'VIDEO_BATCH':
         return publish(() => mergeVideoBatch(message.username, message.profileUrl, message.videos));
+      case 'TAG_SCAN_BEGIN':
+        return publish(() => beginTagResearch(message.tag, message.tagUrl, message.options));
+      case 'TAG_VIDEO_BATCH':
+        return publish(() => mergeTagVideoBatch(message.tag, message.tagUrl, message.videos));
       case 'SCAN_STATE':
         return publish(() => updateScanState(message.state));
       case 'CLEAR_PROFILE':
         return publish(() => clearProfile(message.username));
+      case 'CLEAR_TAG_RESEARCH':
+        return publish(() => clearTagResearch(message.tag));
       default:
         return undefined;
     }
