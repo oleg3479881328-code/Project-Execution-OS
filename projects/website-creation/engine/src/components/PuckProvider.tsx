e8e42daca31b8cdb5e4cb4420307c780721966b6
oleg3479881328-code <oleg3479881328@gmail.com>
2026-09-14 19:50:04 -0400

    Wire proven image editor plugin into shared Puck provider

--- projects/website-creation/engine/src/components/PuckProvider.tsx
+++ projects/website-creation/engine/src/components/PuckProvider.tsx
@@ -2,7 +2,12 @@
 
 import { PuckConfigProvider } from '@delmaredigital/payload-puck/client'
 import { websiteEditorConfig } from '@/puck/editor-config'
+import { imageEditorPlugin } from '@/puck/image-editor/image-editor-plugin'
 
 export default function PuckProvider({ children }: { children: React.ReactNode }) {
-  return <PuckConfigProvider config={websiteEditorConfig}>{children}</PuckConfigProvider>
+  return (
+    <PuckConfigProvider config={websiteEditorConfig} plugins={[imageEditorPlugin]}>
+      {children}
+    </PuckConfigProvider>
+  )
 }
