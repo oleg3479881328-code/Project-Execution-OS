4304a3b2df1906da63f398f9cb2295151c2fe363
oleg3479881328-code <oleg3479881328@gmail.com>
2026-09-14 13:44:55 -0400

    Add media picker only in client-side Website Creator editor

--- projects/website-creation/engine/src/puck/editor-config.ts
+++ projects/website-creation/engine/src/puck/editor-config.ts
@@ -1,11 +1,23 @@
 'use client'
 
 import { editorConfig, extendConfig } from '@delmaredigital/payload-puck/config/editor'
-import { websiteComponents, websiteComponentNames } from './website-components'
+import { createMediaField } from '@delmaredigital/payload-puck/fields'
+import { HeroSectionConfig, websiteComponents, websiteComponentNames } from './website-components'
+
+const editorWebsiteComponents = {
+  ...websiteComponents,
+  HeroSection: {
+    ...HeroSectionConfig,
+    fields: {
+      ...HeroSectionConfig.fields,
+      image: createMediaField({ label: 'Hero image' }),
+    },
+  },
+}
 
 export const websiteEditorConfig = extendConfig({
   base: editorConfig,
-  components: websiteComponents,
+  components: editorWebsiteComponents,
   categories: {
     website: {
       title: 'Website Sections',
